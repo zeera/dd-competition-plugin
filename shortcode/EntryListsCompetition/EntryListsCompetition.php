15b11b035825ba14dad6a6608c5434df3462697c
alyzone <ccm.aliyap@gmail.com>
2022-05-06 20:15:28 +0800

    get latest updates from repo

--- shortcode/EntryListsCompetition/EntryListsCompetition.php
+++ shortcode/EntryListsCompetition/EntryListsCompetition.php
@@ -9,6 +9,22 @@ namespace WpDigitalDriveCompetitions\Shortcode\EntryListsCompetition;
 class EntryListsCompetition
 {
     public static function display($attr) {
-
+        $query = new \WP_Query( array(
+            'post_type'           => 'product',
+            'post_status'         => 'publish',
+            'ignore_sticky_posts' => 1,
+            'posts_per_page'      => $per_page,
+            'orderby'             => $orderby,
+            'order'               => $order,
+            'meta_query' => array(
+                'relation' => 'AND',
+                    array(
+                        'key' => '_end_date_and_time',
+                        'value' => date(),
+                        'compare' => '>=',
+                        'type' => 'datetime'
+                    ),
+            )
+        ) );
     }
 }
