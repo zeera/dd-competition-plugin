64f1d73d1f5a0558b62faf0b27b2e48adedae373
jerome <jerome.diamante01@gmail.com>
2022-05-17 19:34:59 +0800

    added entry lists single page

--- shortcode/EntryListsCompetition/EntryListsCompetition.php
+++ shortcode/EntryListsCompetition/EntryListsCompetition.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 namespace WpDigitalDriveCompetitions\Shortcode\EntryListsCompetition;
-
+use WpDigitalDriveCompetitions\Models\TicketNumbers;
 /**
  *Show Entry Lists Competition (Entry lists will be displayed once the competition closes.) -- parameters are per_page='6', orderby='id', order='desc' & heading_title='Entry Lists Competition'
  */
@@ -12,24 +12,31 @@ class EntryListsCompetition
         ob_start();
         extract($attr);
 
-        $query = new \WP_Query( array(
-            'post_type'           => 'product',
-            'post_status'         => 'publish',
-            'ignore_sticky_posts' => 1,
-            'posts_per_page'      => $per_page,
-            'orderby'             => $orderby,
-            'order'               => $order,
-            'meta_query' => array(
-                'relation' => 'AND',
-                    array(
-                        'key' => '_end_date_and_time',
-                        'value' => date('Y-m-d H:i:s'),
-                        'compare' => '>=',
-                        'type' => 'datetime'
-                    ),
-            )
-        ) );
+        $isSingle = false;
+        if(isset($_GET['competition_name']) && !empty($_GET['competition_name'])) {
+            $isSingle = true;
+        }
 ?>
+    <?php if(!$isSingle): ?>
+        <?php
+            $query = new \WP_Query( array(
+                'post_type'           => 'product',
+                'post_status'         => 'publish',
+                'ignore_sticky_posts' => 1,
+                'posts_per_page'      => $per_page,
+                'orderby'             => $orderby,
+                'order'               => $order,
+                'meta_query' => array(
+                    'relation' => 'AND',
+                        array(
+                            'key' => '_end_date_and_time',
+                            'value' => date('Y-m-d H:i:s'),
+                            'compare' => '<=',
+                            'type' => 'datetime'
+                        ),
+                )
+            ) );
+        ?>
         <div class='entry-lists-competition-section'>
             <div class="entry-lists-competition-heading">
                 <h2 class="section-title"><?= $heading_title ?></h2>
@@ -41,7 +48,7 @@ class EntryListsCompetition
                             $product = wc_get_product( get_the_id() );
                         ?>
                         <div class="entry-list-item">
-                            <a href="<?= get_the_permalink(); ?>" class="entry-list-box">
+                            <a href="<?= get_permalink( get_page_by_path( 'entry-lists' ) ).'?competition_name='.$product->slug; ?>" class="entry-list-box">
                                 <div class="entry-list-feat-img">
                                     <?= wp_get_attachment_image( $product->get_image_id(), 'full' ); ?>
                                 </div>
@@ -66,6 +73,41 @@ class EntryListsCompetition
                 </div>
             <?php endif; ?>
         </div>
+    <?php else:  ?>
+        <?php
+            $product = get_page_by_path( $_GET['competition_name'], OBJECT, 'product' );
+            $ticketNumbersModel = new TicketNumbers;
+            $ticketNumbers = $ticketNumbersModel->getProductEntryList( $product->ID );
+        ?>
+        <div class="entry-list-single-page">
+            <div class="entry-list-single-heading"><h1>Entry Lists For <?= $product->post_title; ?></h1></div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Ticket No.</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if($ticketNumbers): ?>
+                        <?php foreach($ticketNumbers as $key => $tmpData): ?>
+                            <?php
+                                $userData = get_userdata( $tmpData['userid'] );
+                            ?>
+                            <tr>
+                                <td><?php esc_attr_e($tmpData['ticket_number'], 'WpAdminStyle'); ?></td>
+                                <td><?php esc_attr_e( $userData->first_name . ' ' . $userData->last_name, 'WpAdminStyle' ); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <tr>
+                            <td colspan="2">No Entry List</td>
+                        </tr>
+                    <?php endif; ?>
+                </tbody>
+            </table>
+        </div>
+    <?php endif; ?>
 <?php
         $output = ob_get_clean();
         return $output;
